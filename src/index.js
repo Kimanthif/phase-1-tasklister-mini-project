cc65d68f004721f2d028636b86049ea8a9b011df
Felistus Kimanthi <kimanthi.felistus@gmail.com>
2025-04-18 16:17:11 +0300

    tasks added with preventDefault setting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskDescription = document.querySelector(
+      "#new-task-description"
+    ).value;
+
+    const li = document.createElement("li");
+    li.textContent = taskDescription;
+    taskList.appendChild(li);
+
+    form.reset();
+  });
 });
